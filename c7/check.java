20ec094ce6a5c77ec530a9a0c8af5f1cdf18986b
Sandeep Bhasma Tharu <np01cp4a250198@islingtoncollege.edu.np>
2025-12-26 22:51:18 +0545

    check

---
+++ c7/check.java
@@ -0,0 +1,17 @@
+package c7;
+
+
+/**
+ * Write a description of class check here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class check
+{
+    public static void main(String []args)
+    {
+        System.out.println("checking  i am entered in git hub or not");
+        
+    }
+}
